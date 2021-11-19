d8f93c6bac1e751ce76ba7743c8207198969fa76
stbaeumer <stefan.baeumer@berufskolleg-borken.de>
2021-11-19 08:19:47 +0100

    ...

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log('Server lauscht auf Port %s', server.address().port)    
+})
+
+app.get('/',(req, res, next) => {              
+    res.render('index.ejs', {})          
+})
+
