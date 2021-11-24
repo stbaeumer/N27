77685da259958c0ad5b147c6b165559d15c7a181
stbaeumer <stefan.baeumer@berufskolleg-borken.de>
2021-11-24 14:13:42 +0100

    ...

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const server = app.listen(process.env.PORT || 3000, () => {
     console.log('Server lauscht auf Port %s', server.address().port)    
 })
 
-app.get('/',(req, res, next) => {              
+app.get('/index',(req, res, next) => {              
     res.render('index.ejs', {})          
 })
 
