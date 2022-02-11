2c8e3bdeb429ff05216343ecc2aa6013baccfb50
Stefan Bäumer <stbaeumer@users.noreply.github.com>
2022-02-11 11:00:36 +0100

    Übungen zu if und Else

--- Uebungen/klasseUndObjekt.js
+++ Uebungen/klasseUndObjekt.js
@@ -1,4 +1,5 @@
-// Übungen zu Klasse und Objekt
+console.log("Übungen zu Klasse und Objekt")
+console.log("============================") 
 
 // Übung 1
 // In einem Fußballverein sollen Spieler verwaltet werden. 
@@ -7,6 +8,9 @@
 // c) Instanzieren Sie ein Objekt der Klasse
 // d) Initialisieren Sie das Objekt mit Eigenschaftswerten
 
+// zu a) Das Objekt der realen Welt ist der Spieler. 
+
+// zu b)
 class Spieler{
     constructor(){
         this.Name
@@ -16,25 +20,68 @@ class Spieler{
     }
 }
 
+// zu c)
+// Es wird nun ein konkreter Spieler mit konkreten Eigenschaftswerten erzeugt.
+// Dazu wird der konkrete Spieler deklariert (=bekanntgemacht): let spielerMueller
+// In einem zweiten Schritt wird der konkrete Spieler instanziiert = new Spieler()
 let spielerMueller = new Spieler()
+
+// zu d)
+// Es werden konkrete Eigenschaftswerte in den Arbeitsspeicher geschrieben:
 spielerMueller.Name = "Thomas Müller"
 spielerMueller.Nummer = 25
 spielerMueller.Position = "Stürmer"
 spielerMueller.Verein = "FCB"
+spielerMueller.Alter = 18
 
+if(spielerMueller.Alter >= 18){
+    spielerMueller.Volljaehrig = true
+    console.log("Der Spieler " + spielerMueller.Name + " ist volljährig.")
+}
 
-
+console.log(spielerMueller.Name)
+console.log(spielerMueller.Position)
+console.log("Der Spieler " + spielerMueller.Name + " hat die Nummer " + spielerMueller.Nummer + ".")
 
 // Übung 2
-// In einem Schulprogramm sollen Zeugnisse verwaltet werden. 
+// In einem Schulverwaltungsprogramm sollen Zeugnisse verwaltet werden. 
 // a) Identifizieren Sie das Objekt der realen Welt mit seinen relevanten Eigenschaften
 // b) Erstellen Sie die Klassendefinition
 // c) Instanzieren Sie ein Objekt der Klasse
 // d) Initialisieren Sie das Objekt mit Eigenschaftswerten
 
+// zu a) Das "Zeugnis" ist das Objekt der realen Welt.
 
+// zu b)
+class Zeugnis{
+    constructor(){
+        this.SchuelerName
+        this.Klasse
+        this.Geburtsdatum
+        this.Gesamtnote
+        this.Fehlstunden
+        this.Faecher
+    }
+}
 
+// zu c)
+let zeugnisPit = new Zeugnis()
+let zeugnisMax = new Zeugnis()
 
+// zu d)
+zeugnisPit.SchuelerName = "Pit Kiff"
+zeugnisPit.Fehlstunden = 100
+zeugnisPit.Gesamtnote = 1
+
+zeugnisMax.SchuelerName = "Max Muster"
+zeugnisMax.Fehlstunden = 10
+zeugnisMax.Gesamtnote = 2
+
+if(zeugnisMax.Fehlstunden > zeugnisPit.Fehlstunden){
+    console.log("Max Muster hat mehr Fehlstunden") 
+}else{
+    console.log("Pit Kiff hat mehr Fehlstunden")
+}
 
 
 // Übung 3
