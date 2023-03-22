e158f6e77b3dc1c112359e4dc8da6f5fe7d8ceae
Stefan Bäumer <stbaeumer@users.noreply.github.com>
2023-03-22 11:29:42 +0000

    ...

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var dbVerbindung = mysql.createConnection({
     // dann muss geprüft werden, ob der Datenbank-Dienst auf dem Rechner läuft. Dazu
     // melden wir uns auf dem Datenbankserver an und starten die MySQL-Workbench.
 
-    host: "10.40.38.110",
+    host: "130.255.124.99",
     user: "placematman",
     password: "BKB123456!",
     database: "dbn27"
